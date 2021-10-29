066948f33c8832ac8c334cec962c3629b01e026e
Mehtap Uğur <yaziliminval@hotmail.com>
2021-10-29 22:45:17 +0300

    Add combination result

--- salesman.js
+++ salesman.js
@@ -47,3 +47,79 @@ function calcDistance(points) {
   }
   return sum;
 }
+
+
+
+/* ///////////////////////// BULDUĞUM COMBİNASYON ÇÖZÜMÜ
+<script>
+
+// Javascript program to print all
+// combination of size r in an array of size n
+	
+	/* arr[] ---> Input Array
+	data[] ---> Temporary array to
+	store current combination
+	start & end ---> Staring and
+	Ending indexes in arr[]
+	index ---> Current index in data[]
+	r ---> Size of a combination to be printed */
+	function combinationUtil(arr,n,r,index,data,i)
+	{
+		// Current combination is ready
+		// to be printed, print it
+		if (index == r)
+		{
+			for (let j=0; j<r; j++)
+			{
+				document.write(data[j]+" ");
+			}
+			document.write("<br>");
+		
+			return;
+		}
+		
+		// When no more elements are there
+		// to put in data[]
+		if (i >= n)
+		{
+			return;
+		}
+		
+		// current is included, put
+		// next at next location
+		data[index] = arr[i];
+		combinationUtil(arr, n, r, index+1, data, i+1);
+
+		// current is excluded, replace
+		// it with next (Note that
+		// i+1 is passed, but index is not changed)
+		combinationUtil(arr, n, r, index, data, i+1);
+		
+	}
+	
+	// The main function that prints
+	// all combinations of size r
+	// in arr[] of size n. This function
+	// mainly uses combinationUtil()
+	function printCombination(arr,n,r)
+	{
+		// A temporary array to store
+		// all combination one by one
+		let data=new Array(r);
+		
+		// Print all combination using
+		// temporary array 'data[]'
+		combinationUtil(arr, n, r, 0, data, 0);
+	}
+	
+	/*Driver function to check for above function*/
+	let arr=[1, 2, 3, 4, 5];
+	let r = 3;
+	let n = arr.length;
+	printCombination(arr, n, r);
+	
+	// This code is contributed by avanitrachhadiya2155
+	
+</script>
+
+*/
